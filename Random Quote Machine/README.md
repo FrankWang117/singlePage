0890cb7034a2648398479b4c83a13306e1f20a0c
Tomorrowan <wsgoodman@163.com>
2017-08-16 18:45:06 +0800

    rewrite share button function
    重写weibo分享功能。

--- Random Quote Machine/README.md
+++ Random Quote Machine/README.md
@@ -1,2 +1,3 @@
 ## Random Quote Machine  
-页面功能：点击 change 按钮可以随机从服务器抓取一个句子，以及作者。并且可以点击 `weibo` 按钮将页面分享到网页中去
+页面功能：点击 change 按钮可以随机从服务器抓取一个句子，以及作者。并且可以点击 `weibo` 按钮将页面分享到网页中去.  
+DEMO:[Random Quote](https://frankwang1991.github.io/singlePage/Random%20Quote%20Machine/index.html "Random quote")
--- Random Quote Machine/index.js
+++ Random Quote Machine/index.js
@@ -1,20 +1,22 @@
 $(document).ready(function() {
-	var content = '我从来都不是什么天才，只是一个普通人，是一个连一个小女孩都保护不了的普通的、渺小的人类啊。';
-	var author = '钢之炼金术师';
+	// var content = '';
+	// var author = '';
 	var url = "https://sslapi.hitokoto.cn/?encode=json";
 	var getQuote = function() {
+		$.ajaxSettings.async = false;
 		$.getJSON(url, function(json) {
-			content = json['hitokoto'];
-			author = json["from"];
+			var content = json['hitokoto'];
+			var author = json["from"];
+			$('#quote-p').text(content);
+			$('#author').text(author);
+			$('#weibo').off();
+			$('#weibo').on('click', function() {
+				window.open('http://service.weibo.com/share/share.php?url=""&title=' + content)
+			});
 		});
-
-		$('#quote-p').text(content);
-		$('#author').text(author);
-	}
+	};
+	getQuote();
 	$('#change').on('click', function() {
 		getQuote();
 	});
-	$('#weibo').on('click', function() {
-		window.open('http://service.weibo.com/share/share.php?url=""&title=' + content)
-	})
-})
+});
