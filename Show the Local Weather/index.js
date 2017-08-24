84a941161d11d4b8e11de98eb300f80d2cf2e892
Tomorrowan <wsgoodman@163.com>
2017-08-24 21:21:47 +0800

    rebuild weather demo
    重写了查看本地天气界面，使用原生JS的AJAX函数获取json数据。使用bootstrap使其具有响应式设计。

--- Show the Local Weather/index.js
+++ Show the Local Weather/index.js
@@ -1,101 +1,58 @@
-$(document).ready(function() {
-	var localCity;
-	var nation;
-	var localTime;
-	var weacherDecrip;
-	var nowDegree;
-	var suggestion;
-	var userip = '';
-	var url = '';
-	var oneDate;
-	var oneDescription;
-	var oneMax;
-	var oneMin;
-	var twoDate;
-	var twoDescription;
-	var twoMax;
-	var twoMin;
-	var threeDate;
-	var threeDescription;
-	var threeMax;
-	var threeMin;
-	//获取用户ip
-	$.getJSON("https://weixin.jirengu.com/weather/ip", function(useIp) {
-		userip = useIp.data;
-		url = 'https://free-api.heweather.com/v5/weather?city=' + userip + '&key=24f75f285e1946289fe5b8af7d2abf64';
-		//进行当前天气状况输出
-		$.getJSON(url, function(weathers) {
+var localTime = new Date();
+document.getElementById('local-time').innerHTML = localTime.toLocaleDateString();
+var xmlhttp;
+if (window.XMLHttpRequest) {
+	xmlhttp = new XMLHttpRequest();
+} else {
+	xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+};
 
-			localCity = weathers['HeWeather5'][0]["basic"]["city"];
+xmlhttp.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+		myAdress = JSON.parse(this.responseText);
+		var ipAd = myAdress.data;
+		// document.getElementById('ipadress').innerHTML = myAdress.data;
+		xmlhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				myWeather = JSON.parse(this.responseText);
+				var weatherInfo = myWeather.HeWeather5[0];
+				// 获取城市信息
+				document.getElementById('local-p').innerHTML = weatherInfo.basic.city;
+				//获取温度
+				document.getElementById('nowDegree').innerHTML = weatherInfo.now.tmp + '℃';
+				//获取天气描述
+				document.getElementById('cond').innerHTML = weatherInfo.now.cond.txt;
+				// 插入天气图片
+				var codeImg = weatherInfo.now.cond.code;
+				document.getElementById('nowImg').innerHTML = "<img src='https://cdn.heweather.com/cond_icon/" + codeImg + ".png'>"
+					//获取风力
+				document.getElementById('wind').innerHTML = weatherInfo.now.wind.dir + ' ' + weatherInfo.now.wind.sc + "级";
+				document.getElementById('hum').innerHTML = "相对湿度:" + weatherInfo.now.hum + '%';
+				// PM2.5
+				document.getElementById('pm25').innerHTML = "PM2.5:" + " " + weatherInfo.aqi.city.pm25;
+				// 空气质量
+				document.getElementById('qlty').innerHTML = "空气质量:" + " " + weatherInfo.aqi.city.qlty;
+				// 生活建议
+				document.getElementById('liftNote-p').innerHTML = weatherInfo["suggestion"]["comf"]['txt'];
 
-			nation = weathers['HeWeather5'][0]["basic"]["cnty"];
+			}
+		}
+		var weatherUrl = 'https://free-api.heweather.com/v5/weather?city=' + ipAd + '&key=24f75f285e1946289fe5b8af7d2abf64';
+		xmlhttp.open("GET", weatherUrl, true);
+		xmlhttp.send();
+	}
+}
 
-			weacherDecrip = weathers['HeWeather5'][0]["now"]["cond"]["txt"];
+var ipUrl = "https://weixin.jirengu.com/weather/ip";
+xmlhttp.open("GET", ipUrl, true);
+xmlhttp.send();
 
-			nowDegree = weathers['HeWeather5'][0]["now"]["tmp"];
 
-			suggestion = weathers['HeWeather5'][0]["suggestion"]["comf"]['txt'];
-			// 当天晚些时候预测
-			oneDate = weathers['HeWeather5'][0]["daily_forecast"][0]['date'];
-
-			oneDescription = weathers['HeWeather5'][0]["daily_forecast"][0]["cond"]["txt_n"];
-
-			oneMax = weathers['HeWeather5'][0]["daily_forecast"][0]['tmp']["max"];
-			oneMin = weathers['HeWeather5'][0]["daily_forecast"][0]['tmp']["min"];
-			// 第二天天气预测
-			twoDate = weathers['HeWeather5'][0]["daily_forecast"][1]['date'];
-
-			twoDescription = weathers['HeWeather5'][0]["daily_forecast"][1]["cond"]["txt_d"];
-
-			twoMax = weathers['HeWeather5'][0]["daily_forecast"][1]['tmp']["max"];
-			twoMin = weathers['HeWeather5'][0]["daily_forecast"][1]['tmp']["min"];
-			// 第三天天气预测
-			threeDate = weathers['HeWeather5'][0]["daily_forecast"][2]['date'];
-
-			threeDescription = weathers['HeWeather5'][0]["daily_forecast"][2]["cond"]["txt_d"];
-
-			threeMax = weathers['HeWeather5'][0]["daily_forecast"][2]['tmp']["max"];
-			threeMin = weathers['HeWeather5'][0]["daily_forecast"][2]['tmp']["min"];
-			// 当前城市
-			$('.local-city').text(localCity);
-			// 当前国家
-			$('.nation').text(nation);
-			// 当前时间
-			$('.local-time').text((new Date()).toLocaleString());
-			//天气描述
-			$('.weather-description').text(weacherDecrip);
-			//当前温度
-			$('.now-degree').text(nowDegree);
-			//天气建议
-			$('.suggestion').text(suggestion);
-			//未来预报
-			//当晚日期
-			// $('.one-date').text(oneDate);
-			//夜间天气
-			$('.one-description').text(oneDescription);
-			//夜间温度max~min
-			$('.one-max').text(oneMax);
-			$('.one-min').text(oneMin);
-			//明日日期
-			$('.two-date').text(twoDate);
-			//明日天气
-			$('.two-description').text(twoDescription);
-			//明日温度max~min
-			$('.two-max').text(twoMax);
-			$('.two-min').text(twoMin);
-			// 后天天气预测
-			//后日日期
-			$('.three-date').text(threeDate);
-			//后日天气
-			$('.three-description').text(threeDescription);
-			//明日温度max~min
-			$('.three-max').text(threeMax);
-			$('.three-min').text(threeMin);
-			console.log(weathers['HeWeather5'][0]["daily_forecast"][1]["cond"]["txt_d"]);
-			console.log(suggestion);
-
-		});
-
-	});
-
-})
+window.onload = function() {
+	var lifeNoteP = document.getElementById('liftNote-p');
+	console.log(document.getElementsByClassName('glyphicon-heart'))
+	document.getElementsByClassName('glyphicon-heart').onClick = function(e) {
+		// lifeNoteP.style.display = "block"
+		alert(e);
+	}
+}
