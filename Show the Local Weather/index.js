f65498264e776959589cc1d118f19c4afd4ac297
Tomorrowan <wsgoodman@163.com>
2017-08-26 01:39:49 +0800

    finish this page
    完成页面

--- Show the Local Weather/index.js
+++ Show the Local Weather/index.js
@@ -33,7 +33,107 @@ xmlhttp.onreadystatechange = function() {
 						// 空气质量
 						document.getElementById('qlty').innerHTML = "空气质量:" + " " + weatherInfo.aqi.city.qlty;
 						// 生活建议
-						document.getElementById('liftNote-p').innerHTML = weatherInfo["suggestion"]["comf"]['txt'];
+						document.getElementById('liftNote-p').innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;" + weatherInfo["suggestion"]["comf"]['txt'];
+						//体感温度
+						document.getElementById('bodytmp-p').innerHTML = "体感温度：" +
+							weatherInfo.now.fl + "℃";
+
+						// 未来几天天气预报
+						// 当日晚间时候
+						//天气图标
+						var forecastInfo = weatherInfo.daily_forecast;
+						var fcOneImg = forecastInfo[0].cond.code_n;
+						document.getElementById('one-img').innerHTML = "<img src='https://cdn.heweather.com/cond_icon/" + fcOneImg + ".png'>";
+						// 高低温度
+						document.getElementById('one-tmp').innerHTML = forecastInfo[0].tmp.max + '℃';
+						// 降水概率
+						document.getElementById('one-rain').innerHTML = "降水概率:" +
+							forecastInfo[0].pop;
+						//风力
+						document.getElementById('one-wind').innerHTML = forecastInfo[0].wind.dir +
+							"-" + forecastInfo[0].wind.sc;
+
+						// 明日天气
+						//天气图标
+						var fcTwoImg = forecastInfo[1].cond.code_n;
+						document.getElementById('two-date').innerHTML = forecastInfo[1].date;
+
+						document.getElementById('two-img').innerHTML = "<img src='https://cdn.heweather.com/cond_icon/" + fcTwoImg + ".png'>";
+						// 高低温度
+						document.getElementById('two-tmp').innerHTML = forecastInfo[1].tmp.max + '℃' + "~" + forecastInfo[1].tmp.min + "℃";
+						// 降水概率
+						document.getElementById('two-rain').innerHTML = "降水概率:" +
+							forecastInfo[1].pop;
+						//风力
+						document.getElementById('two-wind').innerHTML = forecastInfo[1].wind.dir +
+							"-" + forecastInfo[1].wind.sc;
+
+						// 后天天气
+						//天气图标
+						var fcThreeImg = forecastInfo[2].cond.code_n;
+						document.getElementById('three-date').innerHTML = forecastInfo[2].date;
+						document.getElementById('three-img').innerHTML = "<img src='https://cdn.heweather.com/cond_icon/" + fcThreeImg + ".png'>";
+						// 高低温度
+						document.getElementById('three-tmp').innerHTML = forecastInfo[2].tmp.max + '℃' + "~" + forecastInfo[2].tmp.min + "℃";
+						// 降水概率
+						document.getElementById('three-rain').innerHTML = "降水概率:" +
+							forecastInfo[2].pop;
+						//风力
+						document.getElementById('three-wind').innerHTML = forecastInfo[2].wind.dir +
+							"-" + forecastInfo[2].wind.sc;
+
+
+						//手机上天气预告
+						// 未来几天天气预报
+						// 当日晚间时候
+						//天气图标
+						var fcOneImg = forecastInfo[0].cond.code_n;
+						document.getElementById('phone-one-img').innerHTML = "<img src='https://cdn.heweather.com/cond_icon/" + fcOneImg + ".png'>";
+						// 高低温度
+						document.getElementById('phone-one-tmp').innerHTML = forecastInfo[0].tmp.max + '℃';
+						// 降水概率
+						document.getElementById('phone-one-rain').innerHTML = "降水概率:" +
+							forecastInfo[0].pop;
+						//风力
+						document.getElementById('phone-one-wind').innerHTML = forecastInfo[0].wind.dir +
+							"-" + forecastInfo[0].wind.sc;
+
+						// 明日天气
+						//天气图标
+						var fcTwoImg = forecastInfo[1].cond.code_n;
+						document.getElementById('phone-two-date').innerHTML = forecastInfo[1].date;
+
+						document.getElementById('phone-two-img').innerHTML = "<img src='https://cdn.heweather.com/cond_icon/" + fcTwoImg + ".png'>";
+						// 高低温度
+						document.getElementById('phone-two-tmp').innerHTML = forecastInfo[1].tmp.max + '℃' + "~" + forecastInfo[1].tmp.min + "℃";
+						// 降水概率
+						document.getElementById('phone-two-rain').innerHTML = "降水概率:" +
+							forecastInfo[1].pop;
+						//风力
+						document.getElementById('phone-two-wind').innerHTML = forecastInfo[1].wind.dir +
+							"-" + forecastInfo[1].wind.sc;
+
+						// 后天天气
+						//天气图标
+						var fcThreeImg = forecastInfo[2].cond.code_n;
+						document.getElementById('phone-three-date').innerHTML = forecastInfo[2].date;
+						document.getElementById('phone-three-img').innerHTML = "<img src='https://cdn.heweather.com/cond_icon/" + fcThreeImg + ".png'>";
+						// 高低温度
+						document.getElementById('phone-three-tmp').innerHTML = forecastInfo[2].tmp.max + '℃' + "~" + forecastInfo[2].tmp.min + "℃";
+						// 降水概率
+						document.getElementById('phone-three-rain').innerHTML = "降水概率:" +
+							forecastInfo[2].pop;
+						//风力
+						document.getElementById('phone-three-wind').innerHTML = forecastInfo[2].wind.dir +
+							"-" + forecastInfo[2].wind.sc;
+
+						// 手机端生活指数各项数据
+						document.getElementById('life-cw').innerText = weatherInfo["suggestion"]["cw"]['txt'];
+						document.getElementById('life-drsg').innerText = weatherInfo["suggestion"]["drsg"]['txt'];
+						document.getElementById('life-sport').innerText = weatherInfo["suggestion"]["sport"]['txt'];
+						document.getElementById('life-uv').innerText = weatherInfo["suggestion"]["uv"]['txt'];
+
+
 
 					}
 				}
@@ -51,12 +151,20 @@ xmlhttp.send();
 //鼠标hover事件
 window.onload = function() {
 	var lifeNoteP = document.getElementById('liftNote-p');
+	var bodyTmpP = document.getElementById('bodytmp-p');
+	// 生活建议hover
 	document.getElementById('w-suggestion').onmouseover = function() {
 		lifeNoteP.style.display = "block";
-		console.log(lifeNoteP)
 	}
 	document.getElementById('w-suggestion').onmouseout = function() {
-		lifeNoteP.style.display = "none";
-		console.log(lifeNoteP)
+			lifeNoteP.style.display = "none";
+
+		}
+		// 体感温度hover
+	document.getElementById('w-bodytmp').onmouseover = function() {
+		bodyTmpP.style.display = "block";
+	}
+	document.getElementById('w-bodytmp').onmouseout = function() {
+		bodyTmpP.style.display = "none";
 	}
 }
